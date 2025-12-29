25d98bfbafb48b2be7ffd25115b640970c50db84
ejkroon <erikjasperkroon@gmail.com>
2025-12-29 16:57:34 +0100

    Removed defunct functions

--- src/__init__.py
+++ src/__init__.py
@@ -8,8 +8,6 @@ from functions import load_network
 from functions import check_paths
 from functions import load_paths_to_graph
 from functions import compare_assemblages
-from functions import extract_paths
-from functions import extract_link_list
 from functions import generate_control_from_link_list
 from functions import permutation_test
 
@@ -19,8 +17,6 @@ __all__ = ['check_target_node',
              'check_paths',
              'load_paths_to_graph',
              'compare_assemblages',
-             'extract_paths',
-             'extract_link_list',
              'generate_control_from_link_list',
              'permutation_test', 
 ]
